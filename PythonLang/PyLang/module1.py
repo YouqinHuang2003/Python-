d959201939c158554b4f14823689f41c29c71d77
YangJay Huang <160500804+YouqinHuang2003@users.noreply.github.com>
2024-03-30 14:03:13 +0800

    Add files via upload

---
+++ PythonLang/PyLang/module1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#将函数存储在模块中
+#module1
+
+def say_hi(name):
+    print(f"Hi! {name}")
+
+def say_Hello(name):
+    print(f"Hello! {name}")
+
+
+# In[ ]:
+
+
+
+
